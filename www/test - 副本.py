faa1b7cef57b890704040ec9e3f36b48b47ee89f
XuZhiJiang <xuzhijiang@sunniwell.net>
2018-04-02 13:08:50 +0800

    modify

---
+++ www/test - 副本.py
@@ -0,0 +1,13 @@
+import orm
+from models import User, Blog, Comment
+
+def test():
+	yield from orm.create_pool(user='www-data', password='www-data', db='awesome')
+
+	u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+	yield from u.save()
+
+for x in test():
+	pass
+# 可以在MySQL客户端命令行查询，看看数据是不是正常存储到MySQL里面了。
